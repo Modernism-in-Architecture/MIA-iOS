1aeb5b95339d62c5a4d778c4c0fa6d1bd31c6772
Sören Kirchner <sk@tologo.de>
2022-02-24 22:32:36 +0100

    refactor, cleanup, use .task

--- MIAapp/view/shared/MIAActivityIndicator.swift
+++ MIAapp/view/shared/MIAActivityIndicator.swift
@@ -7,45 +7,26 @@
 
 import SwiftUI
 
-//struct MIAActivityIndicator: View {
-//    var body: some View {
-//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-//    }
-//}
-//
-//struct MIAActivityIndicator_Previews: PreviewProvider {
-//    static var previews: some View {
-//        MIAActivityIndicator()
-//    }
-//}
-
 struct MIAActivityIndicator: View {
     
-    @State private var fillPoint = 0.0
-    
-    private var animation: Animation {
-        Animation.easeInOut(duration: 1.0).repeatForever(autoreverses: false)
-    }
+    @State private var endPoint = 0.0
     
     var body: some View {
-        let _ = Self._printChanges()
-        ZStack {
-            Ring (endPoint: fillPoint)
+        return ZStack {
+            Ring (endPoint: endPoint)
                 .stroke(Color.green, lineWidth: 4)
-//                .frame(width: 100, height: 100)
-                .onAppear() {
-                    withAnimation(self.animation) {
-                        self.fillPoint = 1.0
+                .rotationEffect(Angle(degrees: 90))
+                .task {
+                    withAnimation(.easeInOut(duration: 1.0).repeatForever(autoreverses: false)) {
+                        self.endPoint = 1.0
                     }
-            }
-            .rotationEffect(Angle(degrees: 90))
+                }
             Image("mia")
                 .resizable()
                 .scaledToFit()
                 .frame(width: 50)
         }
         .frame(width: 100, height: 100)
-//        .transition(.opacity.animation(.easeInOut(duration: 0.5)))
     }
 }
 
@@ -60,29 +41,14 @@ struct Ring: Shape {
     }
     
     func path(in rect: CGRect) -> Path {
-//        let startPoint = (endPoint > delayPoint) ? (2 * endPoint) : 0
-//        var path = Path()
-//
-//        path.addArc(center: CGPoint(x: rect.size.width/2, y: rect.size.width/2),
-//                    radius: rect.size.width/2,
-//                    startAngle: .normal(startPoint),
-//                    endAngle: .normal(endPoint),
-//                    clockwise: false)
-//        return path
-        
         return Path { path in
             let startPoint = (endPoint > delayPoint) ? (2 * endPoint) : 0
-
-//            let startPoint = 0.0
-//            let endPoint = 0.5
             path.addArc(center: CGPoint(x: rect.size.width/2, y: rect.size.width/2),
                         radius: rect.size.width/2,
                         startAngle: .normal(startPoint),
                         endAngle: .normal(endPoint),
                         clockwise: false)
-            
         }
-        
     }
 }
 
@@ -92,9 +58,9 @@ extension Angle {
     }
 }
 
-struct MIAActivityIndicator_Previews: PreviewProvider {
-    static var previews: some View {
-        MIAActivityIndicator()
-            .scaleEffect(0.5)
-    }
-}
+//struct MIAActivityIndicator_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MIAActivityIndicator()
+//            .scaleEffect(0.5)
+//    }
+//}
