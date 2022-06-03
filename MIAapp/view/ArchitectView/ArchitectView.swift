089a0119cae046d33262f5faab522182ebf55b7e
Sören Kirchner <sk@tologo.de>
2022-06-03 22:49:52 +0200

    refactor

---
+++ MIAapp/view/ArchitectView/ArchitectView.swift
@@ -0,0 +1,29 @@
+//
+//  ArchitectView.swift
+//  MIAapp
+//
+//  Created by Sören Kirchner on 01.06.22.
+//
+
+import SwiftUI
+
+struct ArchitectView: View {
+
+    @StateObject var architectDetailController = ArchitectDetailController()
+    @State var id: Int
+
+    var body: some View {
+        switch architectDetailController.architectDetail {
+        case .loading:
+            MIAActivityIndicator().task {
+                await architectDetailController.fetchData(for: id)
+            }
+        case .success(let detail):
+            ArchitectDetailView(detail: detail)
+        case .error(_):
+            // TODO: Better Error Message
+            Text("Error")
+        }
+    }
+}
+
