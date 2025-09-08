afadafaa4db3d62ca39dcf87404cbe7406900aa4
Sören Kirchner <mail@soeren-kirchner.dev>
2025-09-08 22:24:37 +0200

    [REFACTOR] #62 - Cleanup Code

--- MIAPackages/MIACore/Extensions/String+fromHtmlToAttributed.swift
+++ MIAPackages/MIACore/Extensions/String+fromHtmlToAttributed.swift
@@ -7,25 +7,25 @@
 
 import UIKit
 
-extension String {
-
-    @available(*, deprecated, message: "Please use fromMarkdownToAttributedString")
-    func fromHtmlToAttributed() -> AttributedString {
-        let trimmed = self.trimmingCharacters(in: .whitespacesAndNewlines)
-        guard let attributedString = try? NSMutableAttributedString(data: trimmed.data(using: .unicode)!,
-                                                                    options: [.documentType: NSAttributedString.DocumentType.html],
-                                                                    documentAttributes: nil)
-        else {
-            return AttributedString("ERROR")
-        }
-
-        let font = UIFont.preferredFont(forTextStyle: .body)
-        let textRangeForFont: NSRange = NSMakeRange(0, attributedString.length)
-        attributedString.addAttributes([NSAttributedString.Key.font: font], range: textRangeForFont)
-        attributedString.addAttributes([NSAttributedString.Key.foregroundColor: UIColor.label], range: textRangeForFont)
-        return AttributedString(attributedString)
-    }
-}
+//extension String {
+//
+//    @available(*, deprecated, message: "Please use fromMarkdownToAttributedString")
+//    func fromHtmlToAttributed() -> AttributedString {
+//        let trimmed = self.trimmingCharacters(in: .whitespacesAndNewlines)
+//        guard let attributedString = try? NSMutableAttributedString(data: trimmed.data(using: .unicode)!,
+//                                                                    options: [.documentType: NSAttributedString.DocumentType.html],
+//                                                                    documentAttributes: nil)
+//        else {
+//            return AttributedString("ERROR")
+//        }
+//
+//        let font = UIFont.preferredFont(forTextStyle: .body)
+//        let textRangeForFont: NSRange = NSMakeRange(0, attributedString.length)
+//        attributedString.addAttributes([NSAttributedString.Key.font: font], range: textRangeForFont)
+//        attributedString.addAttributes([NSAttributedString.Key.foregroundColor: UIColor.label], range: textRangeForFont)
+//        return AttributedString(attributedString)
+//    }
+//}
 
 extension String {
     
